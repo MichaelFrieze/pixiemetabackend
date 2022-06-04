72f6a15fa18e5dec434f34d8ffad86b969aeef9c
Michael Frieze <michaelfrieze@icloud.com>
2022-06-03 23:14:33 -0400

    trying some postgres db connection setting to fix connection issue and setup for fly deployment

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -12,6 +12,18 @@ module.exports = ({ env }) => ({
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
       },
     },
-    debug: false,
+    acquireConnectionTimeout: 600000,
+    pool: {
+      min: 2,
+      max: 6,
+      acquireTimeoutMillis: 300000,
+      createTimeoutMillis: 300000,
+      destroyTimeoutMillis: 50000,
+      idleTimeoutMillis: 300000,
+      reapIntervalMillis: 10000,
+      createRetryIntervalMillis: 2000,
+      propagateCreateError: false,
+    },
+    debug: true,
   },
 });
